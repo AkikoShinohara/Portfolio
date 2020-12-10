640c5c98501e9a05aa46883a0b3a165a8ecc0b97
AkikoShinohara <69193404+AkikoShinohara@users.noreply.github.com>
2020-12-10 22:25:14 +0900

    classmが画面に入ってからの高さ50へ変更

--- style.js
+++ style.js
@@ -11,7 +11,7 @@ window.onload = function() {
     var elemPos = $(this).offset().top;
     var scroll = $(window).scrollTop();
     var windowHeight = $(window).height();
-    if (scroll > elemPos - windowHeight + 200){
+    if (scroll > elemPos - windowHeight + 50){
      $(this).addClass('effect-scroll');
     }
    });
